709a9ac85ade0a6e1f485db6a7ad21cc5f72b524
Tal Einat <taleinat@gmail.com>
2013-11-04 10:36:07 +0200

    added comments in find_near_matches

--- fuzzysearch/fuzzysearch.py
+++ fuzzysearch/fuzzysearch.py
@@ -37,23 +37,39 @@ def find_near_matches(subsequence, sequence, max_l_dist=0):
             #last_candidate_init_index = index
 
         for cand in candidates:
+            # if this sequence char is the candidate's next expected char
             if char == subsequence[cand.subseq_index]:
+                # if reached the end of the subsequence, return a match
                 if cand.subseq_index + 1 == _subseq_len:
                     yield Match(cand.start, index + 1, cand.dist)
+                # otherwise, update the candidate and keep it
                 else:
                     new_candidates.append(cand._replace(
                         subseq_index=cand.subseq_index + 1,
                     ))
+
+            # if this sequence char is *not* the candidate's next expected char
             else:
-                if cand.dist == max_l_dist or cand.subseq_index == 0:
+                # we can try skipping a sequence or sub-sequence char, unless
+                # this candidate has already skipped the maximum allowed number
+                # of characters
+                if cand.dist == max_l_dist:
                     continue
+
                 # add a candidate skipping a sequence char
                 new_candidates.append(cand._replace(dist=cand.dist + 1))
+
                 # try skipping subsequence chars
                 for n_skipped in xrange(1, max_l_dist - cand.dist + 1):
+                    # if skipping n_skipped sub-sequence chars reaches the end
+                    # of the sub-sequence, yield a match
                     if cand.subseq_index + n_skipped == _subseq_len:
                         yield Match(cand.start, index + 1, cand.dist + n_skipped)
                         break
+                    # otherwise, if skipping n_skipped sub-sequence chars
+                    # reaches a sub-sequence char identical to this sequence
+                    # char, add a candidate skipping n_skipped sub-sequence
+                    # chars
                     elif subsequence[cand.subseq_index + n_skipped] == char:
                         # add a candidate skipping n_skipped subsequence chars
                         new_candidates.append(cand._replace(
@@ -61,4 +77,8 @@ def find_near_matches(subsequence, sequence, max_l_dist=0):
                             subseq_index=cand.subseq_index + n_skipped,
                         ))
                         break
+                # note: if the above loop ends without a break, that means that
+                # no candidate could be added / yielded by skipping sub-sequence
+                # chars
+
         candidates = new_candidates
