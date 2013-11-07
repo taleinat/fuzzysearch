cf5bbd62691c1361ca790e154dc745fc7dba8e26
Tal Einat <taleinat@gmail.com>
2013-11-07 15:18:13 +0200

    added new search function find_near_matches_with_ngrams()

--- fuzzysearch/fuzzysearch.py
+++ fuzzysearch/fuzzysearch.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 
+
 Candidate = namedtuple('Candidate', ['start', 'subseq_index', 'dist'])
 Match = namedtuple('Match', ['start', 'end', 'dist'])
 
@@ -37,8 +38,11 @@ def find_near_matches(subsequence, sequence, max_l_dist=0):
             #last_candidate_init_index = index
 
         for cand in candidates:
+            next_subseq_chars = subsequence[cand.subseq_index:cand.subseq_index + max_l_dist - cand.dist + 1]
+            idx = next_subseq_chars.find(char)
+
             # if this sequence char is the candidate's next expected char
-            if char == subsequence[cand.subseq_index]:
+            if idx == 0:
                 # if reached the end of the subsequence, return a match
                 if cand.subseq_index + 1 == _subseq_len:
                     yield Match(cand.start, index + 1, cand.dist)
@@ -82,3 +86,100 @@ def find_near_matches(subsequence, sequence, max_l_dist=0):
                 # chars
 
         candidates = new_candidates
+
+
+def _find_all(subsequence, sequence, start_index=0, end_index=None):
+    if isinstance(sequence, basestring):
+        find = sequence.find
+    else:
+        raise TypeError('unsupported sequence type: %s' % type(sequence))
+
+    index = find(subsequence, start_index, end_index)
+    while index >= 0:
+        yield index
+        index = find(subsequence, index + 1, end_index)
+
+def _expand(subsequence, sequence, max_l_dist):
+    if not subsequence:
+        return (0, 0)
+
+    scores = range(max_l_dist + 1) + [None] * (len(subsequence) + 1 - (max_l_dist + 1))
+    new_scores = [None] * (len(subsequence) + 1)
+    min_score = None
+    min_score_idx = None
+
+    #print scores
+    for seq_index, char in enumerate(sequence):
+        new_scores[0] = scores[0] + 1
+        for subseq_index in range(0, min(seq_index + max_l_dist, len(subsequence)-1)):
+            new_scores[subseq_index + 1] = min(
+                scores[subseq_index] + (0 if char == subsequence[subseq_index] else 1),
+                scores[subseq_index + 1] + 1,
+                new_scores[subseq_index] + 1,
+            )
+        subseq_index = min(seq_index + max_l_dist, len(subsequence) - 1)
+        new_scores[subseq_index + 1] = last_score = min(
+            scores[subseq_index] + (0 if char == subsequence[subseq_index] else 1),
+            new_scores[subseq_index] + 1,
+        )
+        if subseq_index == len(subsequence) - 1 and (last_score <= min_score or min_score is None):
+            min_score = last_score
+            min_score_idx = seq_index
+
+        scores, new_scores = new_scores, scores
+        #print scores
+
+    return (min_score, min_score_idx + 1) if min_score is not None and min_score <= max_l_dist else (None, None)
+
+#def _get_best_match(subsequence, sequence_part, max_l_dist):
+#    idx = sequence_part.find(subsequence)
+#    if idx >= 0:
+#        return Match(start=idx, end=idx + len(subsequence), dist=0)
+#
+#    return None
+
+Ngram = namedtuple('Ngram', ['start', 'end'])
+
+def _choose_search_range(subseq_len, seq_len, ngram, max_l_dist):
+    start_index = max(0, ngram.start - max_l_dist)
+    end_index = min(seq_len, seq_len - subseq_len + ngram.end + max_l_dist)
+    return start_index, end_index
+
+def find_near_matches_with_ngrams(subsequence, sequence, max_l_dist):
+    ngram_len = len(subsequence) // (max_l_dist + 1)
+    if ngram_len == 0:
+        raise ValueError('the subsequence length must be greater than max_l_dist')
+
+    ngrams = [
+        Ngram(start, start + ngram_len)
+        for start in range(0, len(subsequence) - ngram_len + 1, ngram_len)
+    ]
+
+    matches = []
+    for ngram in ngrams:
+        start_index, end_index = _choose_search_range(len(subsequence), len(sequence), ngram, max_l_dist)
+        for index in _find_all(subsequence[ngram.start:ngram.end], sequence, start_index, end_index):
+            # try to expand left and/or right according to n_ngram
+            dist_left, left_expand_size = _expand(
+                subsequence[:ngram.start][::-1],
+                sequence[index - ngram.start - max_l_dist:index][::-1],
+                max_l_dist,
+            )
+            if dist_left is None:
+                continue
+            dist_right, right_expand_size = _expand(
+                subsequence[ngram.end:],
+                sequence[index + ngram_len:index - ngram.start + len(subsequence) + max_l_dist],
+                max_l_dist - dist_left,
+            )
+            if dist_right is None:
+                continue
+            assert dist_left + dist_right <= max_l_dist
+            #matches.append(_get_best_match(subsequence, sequence[index - ngram.start - max_l_dist:index - ngram.start + len(subsequence) + max_l_dist], max_l_dist))
+            matches.append(Match(
+                start=index - left_expand_size,
+                end=index + ngram_len + right_expand_size,
+                dist=dist_left + dist_right,
+            ))
+
+    return sorted(set(matches))
--- tests/test_fuzzysearch.py
+++ tests/test_fuzzysearch.py
@@ -1,5 +1,6 @@
 from tests.compat import unittest
-from fuzzysearch.fuzzysearch import find_near_matches, Match
+from fuzzysearch.fuzzysearch import find_near_matches, Match, _expand,\
+    find_near_matches_with_ngrams
 
 
 class TestFuzzySearch(unittest.TestCase):
@@ -44,3 +45,141 @@ class TestFuzzySearch(unittest.TestCase):
         self.assertIn(Match(start=3, end=24, dist=1), matches)
 
         #self.assertEquals(1, len(matches))
+
+
+class TestExpand(unittest.TestCase):
+    def test_identical(self):
+        self.assertEquals((0, 3), _expand('abc', 'abc', 0))
+        self.assertEquals((0, 3), _expand('abc', 'abc', 1))
+        self.assertEquals((0, 3), _expand('abc', 'abc', 2))
+
+    def test_one_missing(self):
+        # first item missing
+        self.assertEquals((1, 3), _expand('abcd', 'bcd', 1))
+        self.assertEquals((1, 3), _expand('abcd', 'bcd', 2))
+
+        # second item missing
+        self.assertEquals((1, 3), _expand('abcd', 'acd', 1))
+        self.assertEquals((1, 3), _expand('abcd', 'acd', 2))
+
+        # last item missing
+        self.assertEquals((1, 3), _expand('abcd', 'abc', 1))
+        self.assertEquals((1, 3), _expand('abcd', 'abc', 2))
+
+    def test_no_result(self):
+        self.assertEquals((None, None), _expand('abc', 'def', 0))
+
+    def test_one_extra(self):
+        self.assertEquals((1, 3), _expand('abcd', 'abd', 1))
+        self.assertEquals((1, 3), _expand('abcd', 'abd', 2))
+
+
+class TestFindNearMatchesWithNgrams(unittest.TestCase):
+    def test_substring(self):
+        substring = 'PATTERN'
+        text = 'aaaaaaaaaaPATTERNaaaaaaaaa'
+        idx = text.find(substring)
+        expected_match = Match(start=10, end=17, dist=0)
+
+        self.assertEquals(
+            [expected_match],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=0)
+        )
+        self.assertEquals(
+            [expected_match],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=1)
+        )
+        self.assertEquals(
+            [expected_match],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=2)
+        )
+
+    def test_one_missing_in_middle(self):
+        substring = 'PATTERN'
+        text = 'aaaaaaaaaaPATERNaaaaaaaaa'
+        expected_match = Match(start=10, end=16, dist=1)
+
+        self.assertEquals(
+            [],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=0)
+        )
+        self.assertEquals(
+            [expected_match],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=1)
+        )
+        self.assertEquals(
+            [expected_match],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=2)
+        )
+
+    def test_one_changed_in_middle(self):
+        substring = 'PATTERN'
+        text = 'aaaaaaaaaaPATtERNaaaaaaaaa'
+        expected_match = Match(start=10, end=17, dist=1)
+
+        self.assertEquals(
+            [],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=0)
+        )
+        self.assertEquals(
+            [expected_match],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=1)
+        )
+        self.assertEquals(
+            [expected_match],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=2)
+        )
+
+    def test_one_extra_in_middle(self):
+        substring = 'PATTERN'
+        text = 'aaaaaaaaaaPATTXERNaaaaaaaaa'
+        expected_match = Match(start=10, end=18, dist=1)
+
+        self.assertEquals(
+            [],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=0)
+        )
+        self.assertEquals(
+            [expected_match],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=1)
+        )
+        self.assertEquals(
+            [expected_match],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=2)
+        )
+
+    def test_one_extra_repeating_in_middle(self):
+        substring = 'PATTERN'
+        text = 'aaaaaaaaaaPATTTERNaaaaaaaaa'
+        expected_match = Match(start=10, end=18, dist=1)
+
+        self.assertEquals(
+            [],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=0)
+        )
+        self.assertEquals(
+            [expected_match],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=1)
+        )
+        self.assertEquals(
+            [expected_match],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=2)
+        )
+
+    def test_one_extra_repeating_at_end(self):
+        substring = 'PATTERN'
+        text = 'aaaaaaaaaaPATTERNNaaaaaaaaa'
+        expected_match = Match(start=10, end=17, dist=0)
+
+        self.assertEquals(
+            [expected_match],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=0)
+        )
+        self.assertEquals(
+            [expected_match],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=1)
+        )
+        self.assertEquals(
+            [expected_match],
+            find_near_matches_with_ngrams(substring, text, max_l_dist=2)
+        )
