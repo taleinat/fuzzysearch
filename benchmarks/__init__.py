54e4b70bf576c800acfdcf559c5ed92a1f226c44
Tal Einat <taleinat@gmail.com>
2014-03-28 13:52:47 +0300

    optimized find_near_matches_generic_ngrams and added has_near_match_generic_ngrams

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -1,3 +1,5 @@
+import random
+
 from fuzzysearch.levenshtein import \
     find_near_matches_levenshtein_linear_programming
 from fuzzysearch.levenshtein_ngram import \
@@ -5,6 +7,9 @@ from fuzzysearch.levenshtein_ngram import \
 from fuzzysearch.susbstitutions_only import \
     find_near_matches_substitutions_ngrams as fnm_substitutions_ngrams, \
     find_near_matches_substitutions_linear_programming
+from fuzzysearch.generic_search import \
+    find_near_matches_generic_linear_programming, \
+    find_near_matches_generic_ngrams, has_near_match_generic_ngrams
 
 
 def fnm_levenshtein_lp(subsequence, sequence, max_l_dist):
@@ -15,12 +20,27 @@ def fnm_substitutions_lp(subsequence, sequence, max_substitutions):
     return list(find_near_matches_substitutions_linear_programming(
         subsequence, sequence, max_substitutions))
 
+def fnm_generic_lp(subsequence, sequence, max_l_dist):
+    return list(find_near_matches_generic_linear_programming(
+        subsequence, sequence, max_l_dist, max_l_dist, max_l_dist, max_l_dist))
+
+def fnm_generic_ngrams(subsequence, sequence, max_l_dist):
+    return list(find_near_matches_generic_ngrams(
+        subsequence, sequence, max_l_dist, max_l_dist, max_l_dist, max_l_dist))
+
+def hnm_generic_ngrams(subsequence, sequence, max_l_dist):
+    return has_near_match_generic_ngrams(
+        subsequence, sequence, max_l_dist, max_l_dist, max_l_dist, max_l_dist)
+
 
 search_functions = {
     'levenshtein_lp': fnm_levenshtein_lp,
     'levenshtein_ngrams': fnm_levenshtein_ngrams,
     'substitutions_lp': fnm_substitutions_lp,
     'substitutions_ngrams': fnm_substitutions_ngrams,
+    'generic_lp': fnm_generic_lp,
+    'generic_ngrams': fnm_generic_ngrams,
+    'has_match_generic_ngrams': hnm_generic_ngrams,
 }
 
 benchmarks = {
@@ -29,18 +49,27 @@ benchmarks = {
         sequence = '"ATCG" * (10**3)',
         max_dist = 1,
     ),
+    'random_kevin': dict(
+        subsequence = ''.join(random.choice('ATCG') for _i in xrange(36)),
+        sequence = ''.join(random.choice('ATCG' * 5 + 'N') for _i in xrange(90)),
+        max_dist = 3,
+    ),
 }
 
 
-def run_benchmark(search_func_name, benchmark_name):
+def get_benchmark(search_func_name, benchmark_name):
     search_func = search_functions[search_func_name]
     search_args = dict(benchmarks[benchmark_name])
 
-    if search_func in (fnm_levenshtein_ngrams, fnm_levenshtein_lp):
+    if search_func in (fnm_levenshtein_ngrams, fnm_levenshtein_lp, fnm_generic_lp, fnm_generic_ngrams, hnm_generic_ngrams):
         search_args['max_l_dist'] = search_args.pop('max_dist')
     elif search_func in (fnm_substitutions_ngrams, fnm_substitutions_lp):
         search_args['max_substitutions'] = search_args.pop('max_dist')
     else:
         raise Exception('Unsupported search function: %r' % search_func)
 
+    return search_func, search_args
+
+
+def run_benchmark(search_func, search_args):
     return search_func(**search_args)
