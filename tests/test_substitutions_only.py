d40881ed08450519631d0794b8d593a9287b3d94
Tal Einat <taleinat@gmail.com>
2014-03-29 13:57:17 +0300

    added some tests for has_near_matches_*

--- tests/test_substitutions_only.py
+++ tests/test_substitutions_only.py
@@ -1,6 +1,7 @@
 from fuzzysearch.susbstitutions_only import \
     find_near_matches_substitutions_linear_programming as fnm_subs_lp, \
-    find_near_matches_substitutions_ngrams as fnm_subs_ngrams
+    find_near_matches_substitutions_ngrams as fnm_subs_ngrams, \
+    has_near_match_substitutions_ngrams
 from tests.compat import unittest
 
 from fuzzysearch.common import Match
@@ -213,3 +214,75 @@ class TestFindNearMatchesSubstitionsLinearProgramming(TestSubstitionsOnlyBase, u
 class TestFindNearMatchesSubstitionsNgrams(TestSubstitionsOnlyBase, unittest.TestCase):
     def search(self, subsequence, sequence, max_subs):
         return fnm_subs_ngrams(subsequence, sequence, max_subs)
+
+
+class TestHasNearMatchSubstitionsOnly(unittest.TestCase):
+    def search(self, subsequence, sequence, max_subs):
+        return has_near_match_substitutions_ngrams(subsequence, sequence, max_subs)
+
+    def test_empty_sequence(self):
+        self.assertFalse(self.search('PATTERN', '', max_subs=0))
+
+    def test_empty_subsequence_exeption(self):
+        with self.assertRaises(ValueError):
+            self.search('', 'TEXT', max_subs=0)
+
+    def test_match_identical_sequence(self):
+        self.assertTrue(self.search('PATTERN', 'PATTERN', max_subs=0))
+
+    def test_substring(self):
+        substring = 'PATTERN'
+        text = 'aaaaaaaaaaPATTERNaaaaaaaaa'
+        for max_subs in [0, 1, 2]:
+            self.assertTrue(self.search(substring, text, max_subs))
+
+    def test_double_first_item(self):
+        for max_subs in [0, 1, 2]:
+            self.assertTrue(self.search('def', 'abcddefg', max_subs))
+
+    def test_two_identical(self):
+        for max_subs in [0, 1, 2]:
+            self.assertTrue(self.search('abc', 'abcabc', max_subs))
+            self.assertTrue(self.search('abc', 'abcXabc', max_subs))
+
+    def test_one_changed_in_middle(self):
+        self.assertFalse(self.search('abcdefg', 'abcXefg', 0))
+        self.assertTrue(self.search('abcdefg', 'abcXefg', 1))
+        self.assertTrue(self.search('abcdefg', 'abcXefg', 2))
+
+    def test_one_missing_in_middle(self):
+        substring = 'PATTERN'
+        text = 'aaaaaaaaaaPATERNaaaaaaaaa'
+
+        for max_subs in [0, 1, 2]:
+            self.assertFalse(self.search(substring, text, max_subs=max_subs))
+
+    def test_one_changed_in_middle2(self):
+        substring = 'PATTERN'
+        text = 'aaaaaaaaaaPATtERNaaaaaaaaa'
+
+        self.assertFalse(self.search(substring, text, max_subs=0))
+        self.assertTrue(self.search(substring, text, max_subs=1))
+        self.assertTrue(self.search(substring, text, max_subs=2))
+
+    def test_one_extra_in_middle(self):
+        substring = 'PATTERN'
+        text = 'aaaaaaaaaaPATTXERNaaaaaaaaa'
+
+        for max_subs in [0, 1, 2]:
+            self.assertFalse(self.search(substring, text, max_subs=max_subs))
+
+    def test_dna_search(self):
+        # see: http://stackoverflow.com/questions/19725127/
+        text = ''.join('''\
+            GACTAGCACTGTAGGGATAACAATTTCACACAGGTGGACAATTACATTGAAAATCACAGATTGGT
+            CACACACACATTGGACATACATAGAAACACACACACATACATTAGATACGAACATAGAAACACAC
+            ATTAGACGCGTACATAGACACAAACACATTGACAGGCAGTTCAGATGATGACGCCCGACTGATAC
+            TCGCGTAGTCGTGGGAGGCAAGGCACACAGGGGATAGG
+            '''.split())
+        pattern = 'TGCACTGTAGGGATAACAAT'
+
+        self.assertTrue(self.search(pattern, text, max_subs=2))
+
+    def test_missing_at_beginning(self):
+        self.assertFalse(self.search("ATTEST", "TESTOSTERONE", max_subs=2))
