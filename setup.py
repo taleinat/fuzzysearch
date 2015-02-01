83cb3e819d50029d1fb07943326364a5c6871f44
Tal Einat <taleinat@gmail.com>
2015-02-01 15:37:49 +0200

    fallback when building C extension fails

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import with_statement
 
 import os
 import sys
 
 from distutils.core import setup, Extension
-
+from distutils.command.build_ext import build_ext
+from distutils.errors import CCompilerError, DistutilsExecError, \
+     DistutilsPlatformError
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
@@ -18,6 +21,45 @@ def readfile(file_path):
 
 readme = readfile('README.rst')
 history = readfile('HISTORY.rst').replace('.. :changelog:', '')
+
+
+# fail safe compilation shamelessly stolen from the simplejson
+# setup.py file.  Original author: Bob Ippolito
+
+is_jython = 'java' in sys.platform
+is_pypy = hasattr(sys, 'pypy_version_info')
+
+ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError)
+if sys.platform == 'win32' and sys.version_info > (2, 6):
+    # 2.6's distutils.msvc9compiler can raise an IOError when failing to
+    # find the compiler
+    ext_errors += (IOError,)
+
+
+class BuildFailed(Exception):
+    pass
+
+
+class ve_build_ext(build_ext):
+    """This class allows C extension building to fail."""
+
+    def run(self):
+        try:
+            build_ext.run(self)
+        except DistutilsPlatformError:
+            raise BuildFailed()
+
+    def build_extension(self, ext):
+        try:
+            build_ext.build_extension(self, ext)
+        except ext_errors:
+            raise BuildFailed()
+        except ValueError:
+            # this can happen on Windows 64 bit, see Python issue 7511
+            if "'path'" in str(sys.exc_info()[1]): # works with Python 2 and 3
+                raise BuildFailed()
+            raise
+
 
 _substitutions_only_module = Extension(
     'fuzzysearch._substitutions_only',
@@ -45,34 +87,71 @@ pymemmem_module = Extension(
     include_dirs=['.'],
 )
 
-setup(
-    name='fuzzysearch',
-    version='0.2.2',
-    description='fuzzysearch is useful for finding approximate subsequence matches',
-    long_description=readme + '\n\n' + history,
-    author='Tal Einat',
-    author_email='taleinat@gmail.com',
-    url='https://github.com/taleinat/fuzzysearch',
-    packages=['fuzzysearch'],
-    package_dir={'': 'src'},
-    ext_modules=[_substitutions_only_module, _common_module,
-                 _generic_search_module, pymemmem_module],
-    license="MIT",
-    keywords='fuzzysearch',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: MacOS :: MacOS X',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-)
+
+def run_setup(with_binary=True):
+    ext_modules = [_substitutions_only_module, _common_module,
+                     _generic_search_module, pymemmem_module]
+    if not with_binary:
+        ext_modules = []
+
+    setup(
+        name='fuzzysearch',
+        version='0.2.2',
+        description='fuzzysearch is useful for finding approximate subsequence matches',
+        long_description=readme + '\n\n' + history,
+        author='Tal Einat',
+        author_email='taleinat@gmail.com',
+        url='https://github.com/taleinat/fuzzysearch',
+        packages=['fuzzysearch'],
+        package_dir={'': 'src'},
+        ext_modules=ext_modules,
+        license="MIT",
+        keywords='fuzzysearch',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
+            'Operating System :: MacOS :: MacOS X',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+        cmdclass={'build_ext': ve_build_ext},
+    )
+
+
+def try_building_extension():
+    try:
+        run_setup(True)
+    except BuildFailed:
+        def echo(msg=''):
+            sys.stdout.write(msg + '\n')
+        line = '=' * 74
+        build_ext_warning = 'WARNING: The C extensions could not be ' \
+                            'compiled; speedups are not enabled.'
+
+        echo(line)
+        echo(build_ext_warning)
+        echo('Failure information, if any, is above.')
+        echo('Retrying the build without the C extension now.')
+        echo()
+
+        run_setup(False)
+
+        echo(line)
+        echo(build_ext_warning)
+        echo('Plain-Python installation succeeded.')
+        echo(line)
+
+
+if not (is_pypy or is_jython):
+    try_building_extension()
+else:
+    run_setup(False)
